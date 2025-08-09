3d2480fdbee8a120a43994f6c6749869db8666a4
Iuro Nascimento <iurobpn@proton.me>
2025-08-09 10:43:54 -0300

    add function to open markdown links

---
+++ lua/katu/nvim/mdlinks.lua
@@ -0,0 +1,51 @@
+local M = {}
+function M.get_mdlink()
+    local current_word = vim.fn.expand("<cWORD>")
+    print("Current word: " .. current_word)
+    -- match againt the pattern for obsidian markdown links
+    local pattern = "%[%[([^|]*).*%]%]"
+    local match = string.match(current_word, pattern)
+    if match then
+        local _, _, file = string.find(current_word, pattern)
+        -- if a match is found, return the link
+        -- io.popen("fd " .. file " -H 2>&1")
+        -- use fd to search for the file in the current directory
+        local handle = io.popen("fd " .. file .. " -H -tf 2>&1")
+        local res = handle:read("*a")
+        if handle == nil then
+            return ""
+        end
+        handle:close()
+        return res
+    else
+        -- if no match is found, return an empty string
+        return ""
+    end
+end
+
+function M.open_mdlink()
+    local link = M.get_mdlink()
+    if link == "" then
+        return
+    end
+    files = require'katu.utils'.split(link, "\n")
+    if #files == 0 then
+        return
+    end
+    if #files > 1 then
+        -- if there are multiple files, ask the user to choose one
+        require'katu.nvim.fzf'.run(
+            {
+                source = files,
+                sink = function(file)
+                    vim.cmd("edit " .. file)
+                end,
+                options = '--prompt="Select files> "'
+            })
+    else
+        vim.cmd("edit " .. files[1])
+    end
+    -- open the link in a new tab
+    
+end
+return M
