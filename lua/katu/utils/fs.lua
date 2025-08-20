b3f6dafa14028c76e68591de1225e30193599b8d
Iuro Nascimento <iurobpn@proton.me>
2025-08-20 11:13:12 -0300

    add type hints to katu.utils.fs module

--- lua/katu/utils/fs.lua
+++ lua/katu/utils/fs.lua
@@ -4,6 +4,9 @@ local utils = require('katu.utils')
 -- Helper function to check if a file exists
 local M = {}
 
+---check if file/folder exists
+---@param fname string
+---@return boolean
 function M.file_exists(fname)
     if type(fname) ~= 'string' then
         print('Filename must be a string', vim.log.levels.ERROR)
@@ -20,7 +23,8 @@ function M.file_exists(fname)
     return true
 end
 
--- get current file in vim/neovim
+---get the filename of the open file in the current buffer
+---@return string
 function M.get_current_file()
     if vim ~= nil then
         return vim.fn.expand('%:p')
@@ -29,16 +33,24 @@ function M.get_current_file()
     end
 end
 
--- get basename from path, the last part of the path without a /
+--- get basename from path, the last part of the path without a /
+---@param path string
+---@return string
 function M.basename(path)
     return M.get_filename(path)
 end
+
+---get the file name from the path
+---@param path string
+---@return string
 function M.get_filename(path)
     local parts = utils.split(path, '/')
     return parts[#parts]
 end
 
--- get a path from a path/file or path
+---get a path from a path/file or path
+---@param filepath string
+---@return string
 function M.get_path(filepath)
     if M.is_dir(filepath) then
         return filepath
@@ -51,7 +63,9 @@ function M.get_path(filepath)
     return out
 end
 
--- get a path from a path/file or path
+---get a path from a path/file or path
+---@param filepath string
+---@return string
 function M.downdir(filepath)
     local is_root = filepath:sub(1,1) == '/'
     if filepath == '/'  then
@@ -67,14 +81,24 @@ function M.downdir(filepath)
     return out
 end
 
+---get the extension of a file
+---@param path string
+---@return string
 function M.get_file_extension(path)
     local parts = utils.split(path, '.')
     return parts[#parts]
 end
 
+---check if path string is a directory
+---@param path string
+---@return boolean
 function M.is_dir(path)
     return lfs.attributes(path, 'mode') == 'directory'
 end
+
+---check if a path is a file
+---@param path string
+---@return boolean
 function M.is_file(path)
     return lfs.attributes(path, 'mode') == 'file'
 end
